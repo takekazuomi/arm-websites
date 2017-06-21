91e81dbdce77fd7c57422995aa6643a19b579d25
Takekazu Omi <takekazuomi@gmail.com>
2017-06-22 05:04:26 +0900

    add branch

--- phpmysql.php
+++ phpmysql.php
@@ -9,6 +9,7 @@ foreach ($_SERVER as $key => $value) {
     if (strpos($key, "MYSQLCONNSTR_") !== 0) {
         continue;
     }
+
     $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
     $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
     $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
